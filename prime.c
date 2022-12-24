418bd562a5ce34d3ffabc35dc8281a5ac216dbf0
Anand Tripathi <96043052+theanandtripathi@users.noreply.github.com>
2022-12-25 00:42:31 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -4,7 +4,7 @@ int main()
 {   int n,i,c=0;
     printf("enter a number and know if it is prime or not");
     scanf("%d",&n);
-    for(i=2;i<=n;i++)
+    for(i=1;i<=n;i++)
     {
       if(n%i==0){
            c++;
