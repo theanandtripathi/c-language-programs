299d06cd5f43e205e779dfbbcea5cf914994b737
Anand Tripathi <96043052+theanandtripathi@users.noreply.github.com>
2022-12-24 23:57:15 +0530

    Add files via upload
    enter a number and get to know whether the number is prime or not.

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{   int n,i,c=0;
+    printf("enter a number and know if it is prime or not");
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+      if(n%i==0){
+           c++;
+       }
+    }
+ if(c==2){
+           printf("entered number is prime");
+       }
+       else{
+           printf("entered number is not prime");
+       }
+    return 0;
+}
